7dcec222973ccc2acd5781cf66f6b3b4f47dfbd1
Michael M. Builov <mbuilov@gmail.com>
2020-09-23 21:35:58 +0300

    add ATTRIBUTE_WPRINTF

--- mscrtx/attributes.h
+++ mscrtx/attributes.h
@@ -24,7 +24,7 @@
 /* Must use MinGW.org/mingw-w64 variants of *printf functions,
    which support c99 "%zu"/"%llu" format specifiers.  */
 #if defined(__USE_MINGW_ANSI_STDIO) && !__USE_MINGW_ANSI_STDIO
-# error Must use MinGW.org/mingw-w64 variants of printf functions
+# error Must use MinGW.org/mingw-w64 variants of printf functions (define _POSIX_C_SOURCE)
 #endif
 
 /* Annotate parameters of printf-like function, e.g.:
@@ -58,6 +58,28 @@
 #  define ATTRIBUTE_PRINTF_PTR(fmt, m, n) ATTRIBUTE_PRINTF(fmt, m, n)
 # else
 #  define ATTRIBUTE_PRINTF_PTR(fmt, m, n)
+# endif
+#endif
+
+/* Annotate parameters of wprintf-like function, e.g.:
+  ATTRIBUTE_WPRINTF(fmt, 2, 3) int my_wprintf(int param, const wchar_t *fmt, ...);
+   or
+  ATTRIBUTE_WPRINTF(fmt, 2, 0) int my_vwprintf(int param, const wchar_t *fmt, va_list ap); */
+#ifndef ATTRIBUTE_WPRINTF
+# if defined(_PREFAST_)
+/* MSVC */
+#  define ATTRIBUTE_WPRINTF(fmt, m, n) _At_(fmt, _Printf_format_string_ _Notnull_)
+# elif defined(__Wformat_mingw_printf)
+/* MinGW.org */
+#  define ATTRIBUTE_WPRINTF(fmt, m, n) /*__Wformat_mingw_printf(m, n)*/ ATTRIBUTE_NONNULL(m)
+# elif defined(__MINGW_PRINTF_FORMAT) && !defined(__clang__)
+/* mingw-w64 */
+#  define ATTRIBUTE_WPRINTF(fmt, m, n) \
+	/*__attribute__ ((__format__ (__MINGW_PRINTF_FORMAT, m, n)))*/ ATTRIBUTE_NONNULL(m)
+# else
+/* generic gcc-compatible comiler (e.g., clang) */
+#  define ATTRIBUTE_WPRINTF(fmt, m, n) \
+	/*__attribute__ ((__format__ (__printf__, m, n)))*/ ATTRIBUTE_NONNULL(m)
 # endif
 #endif
 
